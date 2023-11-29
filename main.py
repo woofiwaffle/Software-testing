dd238423148e809a797ae8e9c1548df17f0be0e3
vlad <vladsokolovskij67@gmail.com>
2023-11-29 20:50:12 +0300

    first commit

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
-user_data = input("Введите данные с клавиатуры: ")
-print("Your data:", user_data)
+input_keys = input()
+key_list = input_keys.split()
+result_dict = {key: True for key in key_list}
+print(result_dict)
